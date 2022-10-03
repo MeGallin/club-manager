fb39f3fabf1f997a6baee7fe1c16b21af49678b1
MeGallin <me@garyallin.uk>
2022-10-03 13:49:03 +0100

    login working

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,8 +20,35 @@ exports.register = async (req, res, next) => {
     });
   }
 };
-exports.login = (req, res, next) => {
-  res.send('Login Route');
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res
+      .status(400)
+      .json({ success: false, error: 'Please provide email and password' });
+  }
+
+  try {
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+      res.status(404).json({ success: false, error: 'Invalid credentials' });
+    }
+
+    const isMatched = await user.matchPasswords(password);
+
+    if (!isMatched) {
+      res.status(404).json({ success: false, error: 'Invalid credentials' });
+    }
+
+    res.status(200).json({
+      success: true,
+      token: 'EFFE$$%£%$%4£"FWfwEf',
+    });
+  } catch (error) {
+    res.status(404).json({ success: false, error: error.message });
+  }
 };
 exports.forgotPassword = (req, res, next) => {
   res.send('Forgot password Route');
