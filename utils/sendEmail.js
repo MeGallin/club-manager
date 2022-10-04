21be8c8ccfcc13f3b2a0cf0ad15faff4731c0a48
MeGallin <me@garyallin.uk>
2022-10-04 14:49:51 +0100

    reset link working

---
+++ utils/sendEmail.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  let transporter = nodemailer.createTransport({
+    host: process.env.MAILER_HOST,
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.MAILER_USER,
+      pass: process.env.MAILER_PW,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.MAILER_FROM,
+    to: options.to,
+    bcc: process.env.MAILER_BCC,
+    subject: options.subject,
+    html: options.html,
+  };
+
+  await transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+};
+
+module.exports = sendEmail;
