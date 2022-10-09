177acb0c7ff66cc0b492e75cc86dc0efac5814bd
MeGallin <me@garyallin.uk>
2022-10-09 12:27:14 +0100

    User FE setup

---
+++ client/src/components/User/UserDisplayPanel.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './UserAdminPanel';
+
+const UserDisplayPanel = () => {
+  return <div>UserDisplayPanel to show USER Summary</div>;
+};
+
+export default UserDisplayPanel;
