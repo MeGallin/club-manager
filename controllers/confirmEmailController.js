35f3228e19c72630e128213049aec110d1dd2666
MeGallin <me@garyallin.uk>
2022-10-11 16:43:47 +0100

    Confirmation email working

---
+++ controllers/confirmEmailController.js
@@ -0,0 +1,32 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const ErrorResponse = require('../utils/errorResponse');
+
+// @description: Confirmation Email
+// @route: GET /confirm-email/:token
+// @access: public
+exports.confirmEmail = async (req, res) => {
+  const decodedToken = jwt.verify(
+    req.params.token,
+    process.env.JWT_SECRET,
+    function (err, decoded) {
+      return decoded.id;
+    },
+  );
+
+  const user = await User.findById(decodedToken);
+
+  if (user) {
+    user.isConfirmed = true;
+    await user.save();
+    if (process.env.NODE_ENV === 'production') {
+      return res.redirect('http://www.yourcorporatememory.com/');
+    } else {
+      return res
+        .status(200)
+        .send({ message: 'Your Account has been Verified.' });
+    }
+  } else {
+    return next(new ErrorResponse('No user found', 404));
+  }
+};
