46af8d12c5360dd9d5d785b4c9792c586ec3c8a6
MeGallin <me@garyallin.uk>
2022-10-13 15:51:59 +0100

    is admin working

--- client/src/store/reducers/adminReducers.js
+++ client/src/store/reducers/adminReducers.js
@@ -2,6 +2,9 @@ import {
   ADMIN_GET_ALL_USERS_FAILURE,
   ADMIN_GET_ALL_USERS_REQUEST,
   ADMIN_GET_ALL_USERS_SUCCESS,
+  ADMIN_IS_ADMIN_FAILURE,
+  ADMIN_IS_ADMIN_REQUEST,
+  ADMIN_IS_ADMIN_SUCCESS,
 } from '../constants/adminConstants';
 
 // GET: ADMIN All USERS details reducer
@@ -16,6 +19,23 @@ export const adminUsersDetailsReducer = (state = {}, action) => {
         allUsers: action.payload,
       };
     case ADMIN_GET_ALL_USERS_FAILURE:
+      return { loading: false, error: action.payload };
+    default:
+      return { ...state };
+  }
+};
+// GET: ADMIN isAdmin reducer
+export const adminIsAdminReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ADMIN_IS_ADMIN_REQUEST:
+      return { loading: true };
+    case ADMIN_IS_ADMIN_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+        isAdmin: action.payload,
+      };
+    case ADMIN_IS_ADMIN_FAILURE:
       return { loading: false, error: action.payload };
     default:
       return { ...state };
