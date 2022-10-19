9b519b995ab090d8f4cc35cb6df801f954edfd50
MeGallin <me@garyallin.uk>
2022-10-19 10:58:48 +0100

    BE- working

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const {
   createProfile,
   getProfile,
+  editProfile,
 } = require('../controllers/profileController');
 const { protect } = require('../middleware/auth');
 
 router.route('/profile/:id').get(protect, getProfile);
 router.route('/profile').post(protect, createProfile);
+router.route('/profile-edit/:id').put(protect, editProfile);
 
 module.exports = router;
