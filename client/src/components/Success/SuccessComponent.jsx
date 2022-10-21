6d48f2d77c070f705a98052cd1f726299ec0fa8b
MeGallin <me@garyallin.uk>
2022-10-21 12:33:53 +0100

    Updated component

--- client/src/components/Success/SuccessComponent.jsx
+++ client/src/components/Success/SuccessComponent.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './SuccessComponent.scss';
 import PropTypes from 'prop-types';
 import 'animate.css';
 
 const SuccessComponent = ({ message }) => {
+  const [clear, setClear] = useState('');
+  useEffect(() => {
+    setTimeout(() => {
+      setClear('clear');
+    }, 6000);
+  }, []);
+
   return (
     <div className=" animate__animated animate__bounceInLeft">
-      <span className="success-component">{message}</span>
+      <span className={`success-component ${clear}`}>{message}</span>
     </div>
   );
 };
