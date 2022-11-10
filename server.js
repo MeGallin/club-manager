b6a3cc4d8f8b9a54ee68f2c861ca26d8bdd5dbe4
MeGallin <me@garyallin.uk>
2022-11-10 13:52:17 +0000

    modified server js

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 const cors = require('cors');
 
-// Connect DB
-connectDB();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -24,11 +21,16 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () =>
-  console.log(`Server is running on port ${PORT}`),
-);
+// Connect DB
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+});
+
+// const server = app.listen(PORT, () =>
+//   console.log(`Server is running on port ${PORT}`),
+// );
 
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Logged error: ${err}`);
-  server.close(() => process.exit(1));
+  // server.close(() => process.exit(1));
 });
