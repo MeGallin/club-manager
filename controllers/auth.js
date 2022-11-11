1942bc657ee06dd70189ae5b0a0a8266afc7667d
MeGallin <me@garyallin.uk>
2022-11-11 16:37:16 +0000

    log reset

--- controllers/auth.js
+++ controllers/auth.js
@@ -94,6 +94,8 @@ exports.forgotPassword = async (req, res, next) => {
     await user.save();
     const resetUrl = `${process.env.RESET_PASSWORD_LOCAL_URL}password-reset/${resetToken}`;
 
+    console.log({ resetUrl });
+
     const message = `<h1>You have requested a password reset.</h1><p>Please click on the following link to reset your password.</p><p><a href=${resetUrl} id='link'>Click here to verify</a></p>`;
 
     try {
