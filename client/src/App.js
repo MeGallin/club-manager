d98e3f541cffb0a4fc0cd8ba0b7c1617df202f5b
MeGallin <me@garyallin.uk>
2022-12-12 11:39:20 +0000

    WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
+import { lazy, Suspense } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
 
-import HeaderComponent from './components/Header/HeaderComponent';
 import HomeView from './views/HomeView/HomeView';
 import FooterComponent from './components/Footer/FooterComponent';
 import { ErrorView } from './views/ErrorView/ErrorView';
 import ContactView from './views/ContactView/ContactView';
-import AboutView from './views/AboutView/AboutView';
+
 import LoginView from './views/LoginView/LoginView';
 import PWResetView from './views/PWRestView/PWResetView';
 import UserAdminView from './views/UserAdminView/UserAdminView';
@@ -17,40 +17,55 @@ import AdminGeneralInformationView from './views/AdminView/AdminGeneralInformati
 import AdminPublicNoticeView from './views/AdminView/AdminPublicNoticeView.jsx';
 import AdminPrivateMessagesView from './views/AdminView/AdminPrivateMessagesView.jsx';
 
+const HeaderComponent = lazy(() =>
+  import('./components/Header/HeaderComponent'),
+);
+const AboutView = lazy(() => import('./views/AboutView/AboutView'));
+
 const App = () => {
   return (
     <Router>
       <div className="container--fluid">
         <HeaderComponent />
         <div className="content-wrapper">
-          <Routes>
-            <Route path="/" element={<HomeView />} />
-            <Route path="/home" element={<HomeView />} exact />
-            <Route path="/about" element={<AboutView />} exact />
-            <Route path="/contact" element={<ContactView />} exact />
-            <Route path="/login" element={<LoginView />} exact />
-            <Route path="/password-reset/:token" element={<PWResetView />} />
-            <Route path="/user-admin" element={<UserAdminView />} exact />
-            <Route path="/admin-profile" element={<AdminProfileView />} exact />
-            <Route path="/admin-players" element={<AdminPlayersView />} exact />
-            <Route
-              path="/admin-private-messages"
-              element={<AdminPrivateMessagesView />}
-              exact
-            />
-            <Route
-              path="/admin-general-information"
-              element={<AdminGeneralInformationView />}
-              exact
-            />
-            <Route
-              path="/admin-public-notice"
-              element={<AdminPublicNoticeView />}
-              exact
-            />
-            <Route path="/admin" element={<AdminView />} exact />
-            <Route path="*" element={<ErrorView />} exact />
-          </Routes>
+          <Suspense fallback={<>Loading...</>}>
+            <Routes>
+              <Route path="/" element={<HomeView />} />
+              <Route path="/home" element={<HomeView />} exact />
+              <Route path="/about" element={<AboutView />} exact />
+              <Route path="/contact" element={<ContactView />} exact />
+              <Route path="/login" element={<LoginView />} exact />
+              <Route path="/password-reset/:token" element={<PWResetView />} />
+              <Route path="/user-admin" element={<UserAdminView />} exact />
+              <Route
+                path="/admin-profile"
+                element={<AdminProfileView />}
+                exact
+              />
+              <Route
+                path="/admin-players"
+                element={<AdminPlayersView />}
+                exact
+              />
+              <Route
+                path="/admin-private-messages"
+                element={<AdminPrivateMessagesView />}
+                exact
+              />
+              <Route
+                path="/admin-general-information"
+                element={<AdminGeneralInformationView />}
+                exact
+              />
+              <Route
+                path="/admin-public-notice"
+                element={<AdminPublicNoticeView />}
+                exact
+              />
+              <Route path="/admin" element={<AdminView />} exact />
+              <Route path="*" element={<ErrorView />} exact />
+            </Routes>
+          </Suspense>
         </div>
         <FooterComponent />
       </div>
